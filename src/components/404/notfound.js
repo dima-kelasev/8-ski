d720a1e5ae682df660e4cc915836b81faad71665
Dima Kelasev <dimakelaseff@gmail.com>
2021-06-07 11:57:51 +0300

    fix 404 page on devices

--- src/components/404/notfound.js
+++ src/components/404/notfound.js
@@ -7,7 +7,7 @@ const NotFound = () => {
   return (
     <>
       <div className='notFound'>
-        <img className='notFound_img' src={notFound} alt=""/>
+
         <div className='notFound_block'>
           <p className='notFound_title'>Ууупс... Произошла ошибка, попробуйте обновить страницу</p>
           <Link className='notFound_Link' to='/'> <button>Обновить</button></Link>
